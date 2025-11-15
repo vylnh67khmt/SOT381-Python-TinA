d92d8c1614c059031a69032492abd7c1c978cf68
lenguyenhavy <vy.lnh.67khmt@ntu.edu.vn>
2025-11-15 18:39:17 +0700

    Delete so_nguyen_lon_nhat.py

--- so_nguyen_lon_nhat.py
+++
@@ -1,7 +0,0 @@
-a,b,c=input("Nhập ba số nguyên bất kì:").split()
-if a>=b and a>=c:
-    print(a," là số lớn nhất")
-elif b>=a and b>=c:
-    print(b," là số lớn nhất")
-else:
-    print(c," là số lớn nhất")
