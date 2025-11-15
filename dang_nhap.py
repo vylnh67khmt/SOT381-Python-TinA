35439594cdb9ecfb6d3db386f17bfa8c5a79f466
lenguyenhavy <vy.lnh.67khmt@ntu.edu.vn>
2025-11-15 18:35:28 +0700

    Delete dang_nhap.py

--- dang_nhap.py
+++
@@ -1,6 +0,0 @@
-username = input("Nhập username:").strip()
-password = input("Nhập password:").strip()
-if username == "admin" and password == "12345":
-    print("Đăng nhập thành công")
-else:
-    print("Đăng nhập thất bại")
