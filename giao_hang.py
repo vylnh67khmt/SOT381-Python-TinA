96d628d5692878e3ac1373c0665485139ff7218e
lenguyenhavy <vy.lnh.67khmt@ntu.edu.vn>
2025-11-15 18:36:54 +0700

    Delete giao_hang.py

--- giao_hang.py
+++
@@ -1,7 +0,0 @@
-gia=float(input("Nhập tổng giá trị đơn hàng (nghìn đồng):"))
-if gia<500:
-    print("Miễn phí giao hàng (0đ)")
-elif gia>=100 and gia<=500:
-    print("Phí giao hàng là 20.000đ")
-else:
-    print("Phí giao hàng là 35.000đ")
