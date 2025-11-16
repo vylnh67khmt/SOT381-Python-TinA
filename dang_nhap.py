23d50ddd4d41f5ca1c7208688e3849cb7578439e
vylnh67khmt <vy.lnh.67khmt@ntu.edu.vn>
2025-11-16 18:42:19 +0700

    Đăng nhập
    Nhập tên và pass, kiểm tra đăng nhập thành công hay không

---
+++ dang_nhap.py
@@ -0,0 +1,6 @@
+username = input("Nhập username:").strip()
+password = input("Nhập password:").strip()
+if username == "admin" and password == "12345":
+    print("Đăng nhập thành công")
+else:
+    print("Đăng nhập thất bại")
