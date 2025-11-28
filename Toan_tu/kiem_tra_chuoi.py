426c9b0cf68bcc311e40f7f356eca4f7aa4ca37b
vylnh67khmt <vy.lnh.67khmt@ntu.edu.vn>
2025-11-28 18:35:47 +0700

    Kiểm tra chuỗi
    Nhập vào một chuỗi, dùng toán tử membership và logic để kiểm tra điều kiện

---
+++ Toan_tu/kiem_tra_chuoi.py
@@ -0,0 +1,3 @@
+chuoi=input("Nhập vào một chuỗi bất kì:")
+print("'Python' và 'Programming' có trong chuỗi vừa nhập:","Python" and "Programming" in chuoi)
+print("'Python' và 'Programming' không có trong chuỗi vừa nhập:","Python" and "Programming" not in chuoi)
