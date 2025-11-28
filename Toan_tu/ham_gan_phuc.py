d5aa4ad6026294af088c5e38aed62a194416af53
vylnh67khmt <vy.lnh.67khmt@ntu.edu.vn>
2025-11-28 18:35:07 +0700

    Sử dụng hàm gán phức để tính toán
    Nhập vào giá trị ban đầu, dùng hàm gán phức để tính toán

---
+++ Toan_tu/ham_gan_phuc.py
@@ -0,0 +1,10 @@
+total=100
+print(f"Giá trị ban đầu là: {100}")
+total-=25
+print(f"Giá trị sau khi trừ 25 là: {total}")
+total*=2
+print(f"Giá trị sau khi nhân 2 là: {total}")
+total/=5
+print(f"Giá trị sau khi chia 5 là: {total:.0f}")
+total+=10
+print(f"Giá trị sau khi cộng thêm 10 là: {total:.0f}")
