c77f91fcb3f03171986264547c809f5949c079d4
vylnh67khmt <vy.lnh.67khmt@ntu.edu.vn>
2025-12-15 06:20:04 +0700

    Kiểm tra số chẳn hay số lẻ
    Nhập vào một số bất kì, in ra kết quả kiểm tra số đó là số chẳn hay số lẻ

---
+++ Vong_lap_For_va_While/chan_le.py
@@ -0,0 +1,8 @@
+n=int(input("Nhập vào một số bất kì:"))
+while True:
+    if n%2==0:
+        print(f"Số vừa nhập là số chẵn")
+        break
+    else: 
+        print(f"Số vừa nhập là số lẻ")
+        break
