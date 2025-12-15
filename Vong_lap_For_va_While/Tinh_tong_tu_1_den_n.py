97fdbdb16170944663ab7ad72836c5a5d65f9a1d
vylnh67khmt <vy.lnh.67khmt@ntu.edu.vn>
2025-12-15 07:21:13 +0700

    Update Tinh_tong_tu_1_den_n.py

--- Vong_lap_For_va_While/Tinh_tong_tu_1_den_n.py
+++ Vong_lap_For_va_While/Tinh_tong_tu_1_den_n.py
@@ -1,9 +1,7 @@
 n = 10
 tong = 0
 print(f"Tính tổng từ 1 đến {n}:")
-
 for i in range(1, n + 1):
     tong = tong + i
     print(f"+ Thêm {i} → Tổng: {tong}")
-
 print(f"Kết quả: 1 + 2 + ... + {n} = {tong}")
