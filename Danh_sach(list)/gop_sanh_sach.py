413779c4dc73f749f803d639a760b34d7d35e2e7
vylnh67khmt <vy.lnh.67khmt@ntu.edu.vn>
2025-12-21 20:16:37 +0700

    Gộp danh sách
    Nhập hai danh sách bất kì, in ra kết quả danh sách gộp từ hai danh sách trên

---
+++ Danh_sach(list)/gop_sanh_sach.py
@@ -0,0 +1,4 @@
+danh_sach_1=list(x for x in input("Nhập danh sách 1:").split())
+danh_sach_2=list(x for x in input("Nhập danh sách 2:").split())
+danh_sach_3=danh_sach_1+danh_sach_2
+print(f"Danh sách sau khi gộp là: {danh_sach_3}")
